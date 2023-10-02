d3fdee58cfd5d58a9c78a42e1ea0f94b22c6fe4b
overlord <rexatuzie@gmail.com>
2023-10-02 18:52:11 +0100

    vncvhmj,k.

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -3,26 +3,28 @@ import { BsEyeSlash } from "react-icons/bs";
 
 const UserCard = () => {
   return (
-    <div className=" bg-green-600 shadow-lg  px-4 py-6 lg:p-8 rounded h-fit mb-10">
+    <div className=" :bg-white shadow-lg  px-4 py-6 lg:p-8 rounded h-fit mb-10">
       <div className=" mb-4 flex justify-between items-center">
         <div>
-          <p className="  font-medium text-sm text-white  ">Current Balance</p>
+          <p className="  font-medium text-sm text-green-600  ">
+            Current Balance
+          </p>
           <h2 className=" text-xl lg:text-4xl font-semibold  text-yellow-500">
             {" "}
             &#8358; 7,233,000
           </h2>
         </div>
 
-        <div className="bg-green-400 text-white  p-2 h-fit rounded-full">
+        <div className="bg-green-600 text-white  p-2 h-fit rounded-full">
           <BsEyeSlash size={20} />
         </div>
       </div>
 
       <div className=" mt-6 lg:mt-8 flex gap-4 ">
-        <button className=" text-white bg-green-400  border-2 border-yellow-500 py-2 lg:py-3 text-center font-medium rounded-md text-sm px-6 lg:px-8">
+        <button className=" text-white bg-green-600  border-2 border-yellow-500 py-2 lg:py-3 text-center font-medium rounded-md text-sm px-6 lg:px-8">
           Send
         </button>
-        <button className="text-white bg-green-400  border-2 border-yellow-500  py-2 lg:py-3 text-center font-medium rounded-md text-sm px-6 lg:px-8">
+        <button className="text-white bg-green-600  border-2 border-yellow-500  py-2 lg:py-3 text-center font-medium rounded-md text-sm px-6 lg:px-8">
           Withdraw
         </button>
       </div>
