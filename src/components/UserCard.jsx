9e61634d7a4b67c0822583b3c93b4ce01958c598
overlord <rexatuzie@gmail.com>
2023-10-06 10:05:19 +0100

    sdghmv,jk

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -6,23 +6,20 @@ const UserCard = () => {
     <div className=" bg-white shadow-lg  px-4 py-6 lg:p-8 rounded h-fit mb-10">
       <div className=" mb-4 flex justify-between items-center">
         <div>
-          <p className="  font-medium text-sm ">Current Balance</p>
-          <h2 className=" text-3xl lg:text-4xl font-bold  t">
-            {" "}
-            &#8358; 7,233,000
-          </h2>
+          <p className="  text-xs  uppercase">current Balance </p>
+          <h2 className=" text-xl lg:text-4xl "> &#8358; 7,233,000</h2>
         </div>
 
-        <div className="bg-black text-white  p-2 h-fit rounded-full">
-          <BsEyeSlash size={20} />
+        <div className="bg-black text-white  p-1 h-fit rounded-full">
+          <BsEyeSlash size={15} />
         </div>
       </div>
 
       <div className=" mt-8 flex gap-4 ">
-        <button className=" text-white bg-black   py-2 lg:py-3 text-center font-medium rounded-sm text-sm px-6 lg:px-8">
-          Send
+        <button className=" text-white bg-black   py-2 lg:py-3 text-center font-medium rounded-lg text-sm px-4 lg:px-8">
+          Invest
         </button>
-        <button className="text-white bg-black   py-2 lg:py-3 text-center font-medium rounded-sm text-sm px-6 lg:px-8">
+        <button className="text-white bg-black   py-2 lg:py-3 text-center font-medium rounded-lg text-sm px-4 lg:px-8">
           Withdraw
         </button>
       </div>
